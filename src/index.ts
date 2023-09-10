ff780a186662d5cb94896fd69eea26ce4f1af0df
zaruous <zaruous@naver.com>
2023-09-10 19:46:42 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express, {Request, Response} from 'express';
+import express, {Request, response, Response} from 'express';
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
@@ -19,8 +19,9 @@ app.get('/req/:url', (req: Request, res: Response) => {
 });
 
 app.get('/async/:id', (req:Request, res : Response) => {
-    performAsyncTask(req.params.id).then(a =>{
-        console.log(`then : ${a}`);
+    performAsyncTask(req.params.id).then( (a : any) => {
+        res.type("text/html");
+        res.send(`then : ${a}`);
     });
 });
 
@@ -28,14 +29,21 @@ app.get('/async/:id', (req:Request, res : Response) => {
 function performAsyncTask(data: string) {
     return new Promise((resolve, reject) => {
         console.log(`received data : ${data}`)
-        resolve(`resolve data : ${data}`);
-        /*
-        setTimeout(() => {
 
+
+        setTimeout(() => {
+            resolve(`resolve data : ${data}`);
         }, 1000); // 1초 후 작업 완료
-         */
+
     });
 }
+
+// Express error handling middleware
+app.use((request : Request, response: Response) => {
+    response.type('text/plain');
+    response.status(505);
+    response.send('Error page');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
